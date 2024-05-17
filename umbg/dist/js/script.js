5bec1315668eee0ad22c816821bb75f9fa7d9343
Misha Morozov <3221vbif@gmail.com>
2024-05-17 05:22:32 +0300

    imbg

--- umbg/dist/js/script.js
+++ umbg/dist/js/script.js
@@ -1,8 +1,58 @@
+var swiper = new Swiper(".slider__slider", {
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 100,
+        depth: 0,
+        modifier: 1,
+        slideShadows: false,
+    },
+    breakpoints: {
+        1441: {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            loop: true,
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 200,
+                depth: 0,
+                modifier: 1,
+                slideShadows: false,
+            },
+        },
+        801: {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            loop: true,
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 100,
+                depth: 0,
+                modifier: 1,
+                slideShadows: false,
+            },
+        }
+    }
+});
+
 let nav = document.querySelector('.nav'),
     navHumburger = document.querySelector('.nav__humburger');
 
 if(nav !== null) {
     navHumburger.addEventListener('click', ()=>{
-        nav.classList.toggle('active')
+        if(document.body.classList.contains('fix')) {
+            nav.classList.remove('active')
+            document.body.classList.remove('fix')
+        } else {
+            nav.classList.add('active')
+            document.body.classList.add('fix')
+        }
     })
 }
