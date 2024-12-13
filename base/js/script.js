22cad481d18152462e9cf713cd740eeefa879cc9
Misha Morozov <3221vbif@gmail.com>
2024-12-13 18:02:07 +0300

    base

---
+++ base/js/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(item=> {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+ 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
