ae7f2a89fa0229f5fb1760cc721e2c983b7b9317
Михаил Морозов <mihailmorozov@MacBook-Air-Mihail.local>
2025-02-26 02:45:05 +0300

    upwork

--- upwork/dist/js/script.js
+++ upwork/dist/js/script.js
@@ -44,17 +44,6 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
-    let select = document.querySelectorAll('.select select'),
-        selectText = document.querySelectorAll('.select p');
-
-    if(select !== null) {
-        select.forEach((item, i)=>{
-            item.addEventListener('change', ()=>{
-                selectText[i].innerHTML = item.value
-            })
-        })
-    }
-
     let nav = document.querySelector('.nav'),
         navHum = document.querySelector('.nav__hum');
 
@@ -108,6 +97,91 @@ document.addEventListener('DOMContentLoaded', function() {
         accBtn.addEventListener('click', (e)=>{
             e.preventDefault();
             accBtn.classList.toggle('active')
+        })
+    }
+
+    let dropInput = document.querySelectorAll('.select input'),
+        dropSelect = document.querySelectorAll('.select ul');
+
+    if(dropInput !== null) {
+        document.body.addEventListener('click',(event)=>{
+            let target = event.target;
+
+            if(target && target.closest('.select')) {
+                return
+            } else {
+                dropInput.forEach(item => item.classList.remove('active'))
+            }
+        })
+
+        dropInput.forEach((item,i)=>{
+            item.addEventListener('click',()=>{
+                item.classList.toggle('active')
+            })
+        })
+
+        dropSelect.forEach((item,i)=>{
+            item.addEventListener('click', (event)=>{
+                let target = event.target;
+                if(target == target && target.closest('li')) {
+                    dropInput[i].classList.remove('active');
+                    dropInput[i].value = target.innerHTML
+                }
+            })
+        })
+    }
+
+    [].forEach.call( document.querySelectorAll('[data-phone-pattern]'), function(input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (9__) ___-__-__",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function(a) {
+                    return i < val.length ? val.charAt(i++) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function(a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+            this.value = new_value;
+            }
+            if (event.type == "blur" && this.value.length < 5) {
+                this.value = "";
+            }
+        }
+    
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false);
+    
+    });
+
+    let inputPassword = document.querySelectorAll('.input__password input'),
+        inputBtn = document.querySelectorAll('.input__password a');
+
+    if(inputPassword !== null) {
+        inputBtn.forEach((item, i)=>{
+            item.addEventListener('click', (e)=>{
+                e.preventDefault()
+                if(inputPassword[i].type == 'password') {
+                    inputPassword[i].setAttribute('type','text');
+                } else {
+                    inputPassword[i].setAttribute('type','password');
+                }
+            })
         })
     }
 
