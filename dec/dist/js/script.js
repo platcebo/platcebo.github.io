ee008579ddea69558c4c83ddaf3eec9bee280c29
misha <misha@MacBook-Air-misha.local>
2025-07-14 15:25:34 +0300

    dec

---
+++ dec/dist/js/script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let catalog = document.querySelector('.nav__drop'),
+    catalogBtn = document.querySelector('.nav__drop_call'),
+    catalogLink = document.querySelectorAll('.nav__drop_list li'),
+    catalogOverflow = document.querySelector('.nav__overflow'),
+    catalogMob = document.querySelector('.nav__mob'),
+    navHum = document.querySelector('.nav__hum'),
+    catalogBlock = document.querySelectorAll('.nav__drop_block');
+
+if(catalog !== null) {
+    catalogBtn.addEventListener('click', ()=>{
+        catalog.classList.toggle('active')
+        catalogOverflow.classList.toggle('active')
+    })
+
+    navHum.addEventListener('click', ()=>{
+        catalogMob.classList.toggle('active')
+        catalogOverflow.classList.toggle('active')
+    })
+
+    catalogOverflow.addEventListener('click', ()=>{
+        catalog.classList.remove('active')
+        catalogOverflow.classList.remove('active')
+        catalogMob.classList.remove('active')
+    })
+
+    catalogLink.forEach((item,i)=>{
+        item.addEventListener('click', ()=>{
+            for(let a=0;a<catalogLink.length; a++) {
+                catalogLink[a].classList.remove('active')
+                catalogBlock[a].classList.remove('active')
+            }
+            catalogLink[i].classList.add('active')
+            catalogBlock[i].classList.add('active')
+        })
+    })
+}
+
+var swiper = new Swiper(".brand__slider", {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    breakpoints: {
+        1201: {
+            slidesPerView: 4,
+        }
+    }
+});
+
+let mediacenterTab = document.querySelectorAll('.mediacenter__tab a'),
+    mediacenterWrapper = document.querySelectorAll('.mediacenter__wrapper');
+
+if(mediacenterTab !== null) {
+    mediacenterTab.forEach((item,i)=>{
+        item.addEventListener('click', (e)=>{
+            e.preventDefault()
+            for(let a=0;a<mediacenterTab.length; a++) {
+                mediacenterTab[a].classList.remove('active')
+                mediacenterWrapper[a].classList.remove('active')
+            }
+            mediacenterTab[i].classList.add('active')
+            mediacenterWrapper[i].classList.add('active')
+        })
+    })
+}
+
+
+
+
+}, false);
