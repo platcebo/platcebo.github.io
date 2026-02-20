3a803b8fa1e10b74298003962bbedd632a82a1b9
misha <misha@MacBook-Air-misha.local>
2026-02-20 18:17:50 +0300

    wwave

--- wave/src/js/script.js
+++ wave/src/js/script.js
@@ -22,6 +22,28 @@ var swiper = new Swiper(".content__wrapper", {
     },
 });
 
+document.querySelectorAll('a[href^="/#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    if (window.location.pathname === '/' || window.location.pathname === '') {
+      e.preventDefault()
+      const target = document.querySelector(this.getAttribute('href').replace('/', ''))
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' })
+      }
+    }
+  })
+})
+
+if(document.querySelector('.footer__row a') !== null ) {
+    let policyLink = document.querySelector('.footer__row a').getAttribute('href');
+
+    let InputLink = document.querySelectorAll('.input__check p a');
+
+    InputLink.forEach((item)=>{
+        item.href = policyLink;
+    })
+}
+
 
 
 
