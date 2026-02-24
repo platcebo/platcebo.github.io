2296f93d24efdadb289e54846eb596264a74dc9a
misha <misha@MacBook-Air-misha.local>
2026-02-25 02:06:23 +0300

    lp

--- LaPateus/src/js/script.js
+++ LaPateus/src/js/script.js
@@ -2,6 +2,28 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-
+var swiper = new Swiper(".card__thumb", {
+    spaceBetween: 20,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+        1025: {
+            // slidesPerView: 2,
+            spaceBetween: 50
+        },
+        601: {
+            // slidesPerView: 2,
+            spaceBetween: 4
+        },
+    }
+});
+var swiper2 = new Swiper(".card__slider", {
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiper,
+    },
+    effect: "fade",
+});
 
 }, false);
